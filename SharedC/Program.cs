4ca14e8098ab3d25382497b506169c1f7804ec5e
Jack Burns <johncburns1@gmail.com>
2019-07-15 15:17:34 -0700

    add .gitignore and fix formatting

--- SharedC/Program.cs
+++ SharedC/Program.cs
@@ -26,90 +26,90 @@ namespace SharedC
 
         static void Main(string[] args)
         {
-                        // CGO checks
-                        // Go code may not store a Go pointer in C memory. 
-                        // C code may store Go pointers in C memory, 
-                       // subject to the rule above: it must stop storing the Go pointer when 
-                        // the C function returns.
+            // CGO checks
+            // Go code may not store a Go pointer in C memory. 
+            // C code may store Go pointers in C memory, 
+            // subject to the rule above: it must stop storing the Go pointer when 
+            // the C function returns.
             Environment.SetEnvironmentVariable("GODEBUG", "cgocheck=2");
             
-                        // define parameters
-                         int a = 10;
-            int b = 2;
-            double x = 100;
-            Int64[] t = new Int64[] { 35, 56, 1, 3, 2, 88, 14 };
+            // define parameters
+            int a       = 10;
+            int b       = 2;
+            double x    = 100;
+            Int64[] t   = new Int64[] { 35, 56, 1, 3, 2, 88, 14 };
 
-                        // Allocate unmanaged memory for
-                        // the GoSlice
-            int n = t.Length;
-            GCHandle h = GCHandle.Alloc(t, GCHandleType.Pinned);
-            GoSlice gs = new GoSlice
+            // Allocate unmanaged memory for
+            // the GoSlice
+            int n       = t.Length;
+            GCHandle h  = GCHandle.Alloc(t, GCHandleType.Pinned);
+            GoSlice gs  = new GoSlice
             {
-                data = h.AddrOfPinnedObject(),
-cap = n,
-len = n
+                data    = h.AddrOfPinnedObject(),
+                cap     = n,
+                len     = n
             };
 
-
-                            // Allocate unmanaged memory for the
-                            // GoString
-                string msg = "I am the Hal 9000";
-           GoString s = new GoString
-            {
-                
-            p = Marshal.StringToHGlobalAnsi(msg),
-n = msg.Length
+            // Allocate unmanaged memory for the
+            // GoString
+            string msg  = "I am the Hal 9000";
+            GoString s  = new GoString
+            {  
+                p       = Marshal.StringToHGlobalAnsi(msg),
+                n       = msg.Length
             };
 
             // call the external functions
-            int addResult = GoMath.Add(a, b);
-            int subResult = GoMath.Sub(a, b);
+            int addResult       = GoMath.Add(a, b);
+            int subResult       = GoMath.Sub(a, b);
             double cosineResult = GoMath.Cosine(x);
-            int sumResult = GoMath.Sum(gs);
-            var helloResult = GoHello.HelloWorld(s);
+            int sumResult       = GoMath.Sum(gs);
+            var helloResult     = GoHello.HelloWorld(s);
             GoMath.Sort(gs);
 
             // Read the Sorted GoSlice
-            n = (int) gs.len;
+            n           = (int) gs.len;
             Int64[] arr = new Int64[n];
-            for (int i = 0; i<n; i++)
+            for (int i  = 0; i < n; i++)
             {
-                arr[i] = Marshal.ReadInt64(gs.data, i* Marshal.SizeOf(typeof(Int64)));
+                arr[i]  = Marshal.ReadInt64(gs.data, i*Marshal.SizeOf(typeof(Int64)));
              }
 
             // Read the size of the data returned by HelloWorld
-            byte* buf = (byte*)helloResult;
+            byte* buf       = (byte*)helloResult;
             byte[] lenBytes = new byte[4];
             for (int i = 0; i< 4; i++)
             {
-                lenBytes[i] = * buf++;
+                lenBytes[i] = *buf++;
             }
 
             // Read the result itself
             n               = BitConverter.ToInt32(lenBytes, 0);
-            int j = 0;
-            byte[] bytes = new byte[n];
-            for (int i = 0; i<n; i++)
+            int j           = 0;
+            byte[] bytes    = new byte[n];
+            for (int i = 0; i < n; i++)
             {
                 if (i< 4)
                 {
-                    * buf++ = 0;
+                    *buf++ = 0;
                 }
                 else
                 {
-                    bytes[j] = * buf++;
+                    bytes[j] = *buf++;
                     j++;
                 }   
             }
 
             // Print results
-            Console.WriteLine("#########################################" +
+            Console.WriteLine(
+                "#########################################" +
                 "\n### .NET Calling Shared-C Golang .dll ###" +
-                "\n#########################################\n");
+                "\n#########################################\n"
+            );
             Console.WriteLine($"HelloWorld: {Encoding.UTF8.GetString(bytes)}");
-             Console.WriteLine($"Add: {addResult}");
-             Console.WriteLine($"Sub: {subResult}");
-             Console.WriteLine($"Cos: {cosineResult}");
+            Console.WriteLine($"Add: {addResult}");
+            Console.WriteLine($"Sub: {subResult}");
+            Console.WriteLine($"Cos: {cosineResult}");
             Console.WriteLine($"Sum: {sumResult}");
             Console.WriteLine(Int64ArrayToString(arr));
 
@@ -125,58 +125,42 @@ n = msg.Length
         {
             var strBuilder = new StringBuilder("");
             var n = arr.Length;
-
-            for (int i = 0; i<n; i++)
+            for (int i = 0; i < n; i++)
              {
                 if (i == (n - 1))
                  {
-
-                   strBuilder  = strBuilder.Append($"{arr[i]}\n");
+                   strBuilder   = strBuilder.Append($"{arr[i]}\n");
                  }
 
                 else if (i == 0)
                  {
-
                     strBuilder  = strBuilder.Append($"Sort: {arr[i]}, ");
                  }
                  else
                  {
-
                     strBuilder  = strBuilder.Append($"{arr[i]}, ");
                  }
              }
-
 
             return strBuilder.ToString();
          }
 
          static class GoMath
         {
-
             [DllImport("math.win.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
-    public static extern int Add(int a, int b);
-
-
+            public static extern int Add(int a, int b);
             [DllImport("math.win.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
-    public static extern int Sub(int a, int b);
-
-
+            public static extern int Sub(int a, int b);
             [DllImport("math.win.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
-        public static extern double Cosine(double x);
-
-
-
+            public static extern double Cosine(double x);
             [DllImport("math.win.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
             public static extern int Sum(GoSlice vals);
-
             [DllImport("math.win.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
             public static extern void Sort(GoSlice vals);
         }
 
         static class GoHello
         {
-
-
             [DllImport("helloworld.win.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
             public static extern unsafe void* HelloWorld(GoString s);
 
